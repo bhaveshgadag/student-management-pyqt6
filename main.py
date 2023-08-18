fe783619a90d8d595f03802e2722599aab8a906f
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-08-18 08:38:49 +0530

    WIP: Status bar

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, QLineEdit, \
-    QComboBox, QPushButton
-from PyQt6.QtGui import QAction
+    QComboBox, QPushButton, QToolBar, QStatusBar
+from PyQt6.QtGui import QAction, QIcon
 from PyQt6.QtCore import Qt
 import sqlite3
 
@@ -10,16 +10,17 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
+        self.setMinimumSize(800, 600)
 
         # File Menu
         file_menu = self.menuBar().addMenu("&File")
-        add_student_action = QAction("Add Student", self)
+        add_student_action = QAction(QIcon("icons/add.png"), "Add Student", self)
         add_student_action.triggered.connect(self.insert)
         file_menu.addAction(add_student_action)
 
         # Edit Menu
         edit_menu = self.menuBar().addMenu("Edit")
-        search_action = QAction("Search", self)
+        search_action = QAction(QIcon("icons/search.png"), "Search", self)
         edit_menu.addAction(search_action)
         search_action.triggered.connect(self.search)
 
@@ -33,6 +34,20 @@ class MainWindow(QMainWindow):
         self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile"))
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
+
+        # Toolbar
+        self.toolbar = QToolBar()
+        self.toolbar.setMovable(True)
+        self.addToolBar(self.toolbar)
+        self.toolbar.addAction(add_student_action)
+        self.toolbar.addAction(search_action)
+
+        # Statusbar
+        edit_button = QPushButton("Edit record")
+        delete_button = QPushButton("Delete record")
+        self.statusBar().addWidget(edit_button)
+        self.statusBar().addWidget(delete_button)
+        edit_button.clicked.connect(self.edit_record)
 
     def load_data(self):
         connection = sqlite3.connect("database.db")
@@ -55,6 +70,9 @@ class MainWindow(QMainWindow):
         dialog = SearchDialog()
         dialog.exec()
 
+    def edit_record(self):
+        dialog = EditDialog()
+        dialog.exec()
 
 class InsertDialog(QDialog):
     def __init__(self):
@@ -140,6 +158,41 @@ class SearchDialog(QDialog):
         cursor.close()
         connection.close()
 
+
+class EditDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Edit student record")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+        index = main_win.table.currentIndex()
+        student_name = main_win.table.item(index, 1).text()
+        print(student_name)
+
+        layout = QVBoxLayout()
+
+        # Student name input
+        self.student = QLineEdit()
+        self.student.setPlaceholderText("Name")
+        layout.addWidget(self.student)
+
+        # Course input
+        self.course_cb = QComboBox()
+        courses = ["Biology", "Astronomy", "Math", "Physics"]
+        self.course_cb.addItems(courses)
+        layout.addWidget(self.course_cb)
+
+        # Mobile input
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile)
+
+        # Submit button
+        button = QPushButton("Add")
+        button.clicked.connect(self.insert_record)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
 
 app = QApplication(sys.argv)
 main_win = MainWindow()
